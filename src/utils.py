ebda75ed983119d75588b7e0de73a7ede038edba
ShatteredXZ <804537158@qq.com>
2025-03-23 18:07:57 +0000

    Update: Utils

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 import os
 import numpy as np
 import open3d as o3d
+import pickle
+import matplotlib.pyplot as plt
 
 
 DATASET_PATHS = ["data/CW2_dataset/harvard_c5/hv_c5_1/",
@@ -12,6 +14,12 @@ DATASET_PATHS = ["data/CW2_dataset/harvard_c5/hv_c5_1/",
          "data/CW2_dataset/mit_76_459/76-459b/"]
 
 NUM_IMAGES = [23, 35, 48, 108, 13, 13, 43]
+
+def get_num_images(dataset_path):
+    """
+    get number of images in the dataset
+    """
+    return len(os.listdir(dataset_path+"image")) - 1
 
 def get_intrinsics(dataset_path):
     """
@@ -26,7 +34,53 @@ def get_data(dataset_path,data_id):
     """
     get rgb, depth, labels
     """
-    # TODO: Check if the data_id is smaller than the number of images in the dataset
+    # Check if data_id is valid
+    if data_id < 0 or data_id > get_num_images(dataset_path):
+        return None, None, None
+    # ================== Load RGB ==================
+    # get name of the i-th image
+    img_name = os.listdir(dataset_path+"image")[data_id]
+    # load the image
+    rgb = plt.imread(dataset_path+"image/"+img_name)
+
+    # ================== Load Depth ==================
+    # get name of the i-th depth image
+    depth_name = os.listdir(dataset_path+"depthTSDF")[data_id]
+    # load the depth image
+    depth = plt.imread(dataset_path+"depthTSDF/"+depth_name)
+
+    # ================== Load Labels ==================
+    with open(dataset_path+"labels/tabletop_labels.dat", 'rb') as label_file:
+        tabletop_labels = pickle.load(label_file)
+        label_file.close()
+    labels = tabletop_labels[data_id]
+    
+    return rgb, depth, labels
+
+def visualize_data(rgb, depth, labels):
+    """
+    visualize rgb, depth, labels
+    """
+    plt.figure(figsize=(15, 5))
+    plt.subplot(131)
+    plt.imshow(rgb)
+    plt.axis('off')
+    plt.title('RGB')
+
+    plt.subplot(132)
+    plt.imshow(depth, cmap='gray')
+    plt.axis('off')
+    plt.title('Depth')
+
+    plt.subplot(133)
+    plt.imshow(rgb)
+    for polygon in labels:
+        plt.plot(polygon[0]+polygon[0][0:1],polygon[1]+polygon[1][0:1],'r')
+    plt.axis('off')
+    plt.title('Labels')
+
+    plt.show()
+
 
 
 ## TODO: depth_to_point_cloud Untested
@@ -61,9 +115,19 @@ def visualize_point_cloud(points):
     o3d.visualization.draw_geometries([pcd])
 
 
-def get_num_images(dataset_path):
-    """
-    get number of images in the dataset
-    """
-    return len(os.listdir(dataset_path+"image")) - 1
 
+
+if __name__ == '__main__':
+    # Load data
+    dataset_path = DATASET_PATHS[0]
+    data_id = 0
+    rgb, depth, labels = get_data(dataset_path, data_id)
+    visualize_data(rgb, depth, labels)
+
+    # Load intrinsics
+    intrinsics = get_intrinsics(dataset_path)
+    print(intrinsics)
+
+    # Convert depth to point cloud
+    points = depth_to_point_cloud(depth, intrinsics)
+    visualize_point_cloud(points)
