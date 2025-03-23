c9bba248746b351e5e68a115dcdced3c669271ad
Yukai <caroyalqi@gmail.com>
2025-03-23 15:57:41 +0000

    update extract, now: rgb+depth_vis_only+depth_raw

--- data/extract.py
+++ data/extract.py
@@ -4,9 +4,19 @@ import cv2
 import os
 
 # 设置路径
-bag_file = "data/realsense/20250321_104523.bag"
-output_dir = "data/realsense/frames_output_20250321_104523"
+bag_file = "data/realsense/20250321_105312.bag"
+output_dir = "data/realsense/frames_output_20250321_105312"
+
 os.makedirs(output_dir, exist_ok=True)
+
+rgb_dir = os.path.join(output_dir, "rgb")
+depth_vis_dir = os.path.join(output_dir, "depth_vis")
+depth_raw_dir = os.path.join(output_dir, "depth_raw")
+
+# 创建子目录
+os.makedirs(rgb_dir, exist_ok=True)
+os.makedirs(depth_vis_dir, exist_ok=True)
+os.makedirs(depth_raw_dir, exist_ok=True)
 
 # 配置 pipeline 从 .bag 文件读取
 pipeline = rs.pipeline()
@@ -17,7 +27,6 @@ config.enable_stream(rs.stream.depth)
 
 # 启动
 pipeline.start(config)
-
 align = rs.align(rs.stream.color)
 frame_id = 0
 
@@ -39,10 +48,17 @@ try:
         depth_image = np.asanyarray(depth_frame.get_data())
 
         # 保存图像
-        cv2.imwrite(f"{output_dir}/frame_{frame_id:03}_rgb.png", color_image)
-        cv2.imwrite(f"{output_dir}/frame_{frame_id:03}_depth.png", depth_image)
+        cv2.imwrite(f"{rgb_dir}/frame_{frame_id:03}.png", color_image)
+
+        # 保存深度图（8-bit） 仅用于可视化
+        depth_vis = cv2.convertScaleAbs(depth_image, alpha=0.03)
+        cv2.imwrite(f"{depth_vis_dir}/frame_{frame_id:03}.png", depth_vis)
+
+        # 保存原始深度图（16-bit）
+        cv2.imwrite(f"{depth_raw_dir}/frame_{frame_id:03}.png", depth_image)
 
         frame_id += 1
+        print(f"frame {frame_id} extracted")
 
 except RuntimeError:
     print("提取结束（视频播放完）")
