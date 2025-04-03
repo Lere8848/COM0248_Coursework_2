f9b7472d9a73a0431030c0e7af3edc050f0b48d9
Yukai <caroyalqi@gmail.com>
2025-04-03 22:02:20 +0100

    Update: Dataloader, add label part loading

--- src/Dataset.py
+++ src/Dataset.py
@@ -2,6 +2,7 @@ import os
 import torch
 import numpy as np
 import torchvision.transforms
+import json
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
 from utils import DATASET_PATHS_MIT, DATASET_PATHS_HARVARD,get_data,get_num_images,depth_to_point_cloud,get_intrinsics
@@ -19,6 +20,22 @@ class PointCloudDataset(Dataset):
         ])
         self.device = device
 
+        self.label_dicts = {}
+        self.image_names_by_path = {}
+        
+        for path in self.data_paths:
+            # load per-dataset label.json from dataset_path
+            label_json_path = os.path.join(path, "label.json")
+            if os.path.exists(label_json_path):
+                with open(label_json_path, "r") as f:
+                    self.label_dicts[path] = json.load(f)
+            
+            # only include image files
+            self.image_names_by_path[path] = sorted([
+                f for f in os.listdir(os.path.join(path, "image"))
+                if f.lower().endswith((".jpg", ".jpeg", ".png"))
+            ])
+
     def __len__(self):
         return self.total_num
     
@@ -35,7 +52,6 @@ class PointCloudDataset(Dataset):
         # Load data
         rgb, depth, labels = get_data(dataset_path, data_id)
         return_dict = {}
-        # TODO: Add polygon data
         if self.data_dict['pointcloud']:
             pointcloud = depth_to_point_cloud(depth, get_intrinsics(dataset_path))
             pointcloud = torch.tensor(pointcloud, dtype=torch.float32).unsqueeze(0).transpose(2, 1)  # shape: [1, 3, n]
@@ -47,7 +63,15 @@ class PointCloudDataset(Dataset):
             rgb = rgb.permute(2, 0, 1)  # Change to (C, H, W)
             return_dict['rgb'] = rgb
         if self.data_dict['labels']:
-            return_dict['labels'] = labels
+            image_names = self.image_names_by_path[dataset_path]
+            if data_id < len(image_names):
+                img_name = image_names[data_id]
+                frame_name = os.path.splitext(img_name)[0]
+            label_dict = self.label_dicts.get(dataset_path, {})
+            polygon = label_dict.get(frame_name, None) # check from polygon_label_dict.json
+            label = 1 if polygon is not None else 0
+            label = torch.tensor([label], dtype=torch.int64) # [1]/[0] to tensor
+            return_dict['labels'] = label
         if self.data_dict['depth']:
             depth = torch.tensor(depth, dtype=torch.float32, device=self.device)
             depth = depth.unsqueeze(0)  # Add channel dimension
