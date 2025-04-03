35e47d8b0480ba3542352e4256bce3dcb353c93a
ShatteredXZ <804537158@qq.com>
2025-04-03 15:24:51 +0100

    Update: Dataloader

--- src/Dataset.py
+++ src/Dataset.py
@@ -1,8 +1,82 @@
 import os
+import torch
 import numpy as np
 import torchvision.transforms
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
-from utils import DATASET_PATHS_MIT, DATASET_PATHS_HARVARD,get_data
+from utils import DATASET_PATHS_MIT, DATASET_PATHS_HARVARD,get_data,get_num_images,depth_to_point_cloud,get_intrinsics
 
+
+class PointCloudDataset(Dataset):
+    def __init__(self, data_paths, data_dict,device=torch.device('cpu')):
+        self.data_paths = data_paths
+        self.data_nums = [get_num_images(path) for path in data_paths]
+        self.total_num = sum(self.data_nums)
+        self.data_dict = data_dict
+        self.transform = torchvision.transforms.Compose([
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize((0.5,), (0.5,))
+        ])
+        self.device = device
+
+    def __len__(self):
+        return self.total_num
+    
+    def __getitem__(self, idx):
+        # Determine which dataset the index belongs to
+        for i, num in enumerate(self.data_nums):
+            if idx < num:
+                dataset_path = self.data_paths[i]
+                data_id = idx
+                break
+            else:
+                idx -= num
+        
+        # Load data
+        rgb, depth, labels = get_data(dataset_path, data_id)
+        return_dict = {}
+        # TODO: Add polygon data
+        if self.data_dict['pointcloud']:
+            pointcloud = depth_to_point_cloud(depth, get_intrinsics(dataset_path))
+            return_dict['pointcloud'] = torch.tensor(pointcloud, dtype=torch.float32, device=self.device)
+        # Apply transformations
+        if self.data_dict['rgb']:
+            rgb = self.transform(rgb)
+            rgb = torch.tensor(rgb, dtype=torch.float32, device=self.device)
+            rgb = rgb.permute(2, 0, 1)  # Change to (C, H, W)
+            return_dict['rgb'] = rgb
+        # if self.data_dict['labels']:
+        #     labels = torch.tensor(labels, dtype=torch.int64, device=self.device)
+        #     return_dict['labels'] = labels
+        if self.data_dict['depth']:
+            depth = torch.tensor(depth, dtype=torch.float32, device=self.device)
+            depth = depth.unsqueeze(0)  # Add channel dimension
+            return_dict['depth'] = depth
+        return return_dict
+        
+def collate_fn(batch):
+    # Collate function to handle variable-length sequences
+    return batch  
+
+if __name__ == "__main__":
+    dataset_paths = [DATASET_PATHS_MIT, DATASET_PATHS_HARVARD]
+    data_dict = {
+        'pointcloud': True,
+        'rgb': True,
+        'depth': True,
+        'labels': True,
+    }
+    
+    dataset = PointCloudDataset(dataset_paths[0], data_dict,device=torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu'))
+    dataloader = DataLoader(dataset, batch_size=4, shuffle=True,collate_fn=collate_fn)
+    
+    for idx, data in enumerate(dataloader):
+        print(f"Batch {idx}:")
+        for key, value in data[0].items():
+            print(f"{key}: {value.shape}")
+        if idx == 1:
+            break
+
+
+        
 
